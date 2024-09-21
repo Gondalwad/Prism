34712e687ecbe7df62071133fdbf652f9f890cd2
gondalwad <sudarshangondalwad@gmail.com>
2024-09-21 15:35:58 +0530

    added redux-persist to persist data

--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -1,97 +1,134 @@
-import React from 'react'
-import Logo from '../components/Logo'
-import {Link, useNavigate} from 'react-router-dom'
-import {TextInput, Button, Label, Alert, Spinner} from 'flowbite-react'
-import { FaGoogle,FaEye } from "react-icons/fa";
-import { useState } from 'react';
+import React from "react";
+import Logo from "../components/Logo";
+import { Link, useNavigate } from "react-router-dom";
+import { TextInput, Button, Label, Alert, Spinner } from "flowbite-react";
+import { FaGoogle, FaEye } from "react-icons/fa";
+import { useState } from "react";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice.js";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SignIn() {
-  // signUP functionality 
+  // signUP functionality
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-
-
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   // saving inputs in json
-  const handleInputs = (e)=>{
-
-    setFormData({...formData, [e.target.id] : e.target.value});
+  const handleInputs = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
     console.log(formData);
-  }
+  };
 
   // sending inputs to backend
-  const handleSubmit = async (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if(!formData.username || !formData.password){
-      return setErrorMessage('Please Fill From Completely');
+    if (!formData.username || !formData.password) {
+      return setErrorMessage("Please Fill From Completely");
     }
-    
 
-    try{
-      setLoading(true);
-      setErrorMessage(null);
+    try {
+      dispatch(signInStart());
 
-      const res = await fetch('/auth/user/signin',{
-        method:'POST',
-        headers:{'Content-Type': 'application/json'},
+      const res = await fetch("/auth/user/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
 
       const data = await res.json();
-
-      if(data.success == false){
-        setLoading(false);
-        console.log(res);
-       return setErrorMessage(data.message);
+      console.log(data);
+      if (data.success == false) {
+        // console.log(res);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
-      
-      res.ok && navigate('/');
 
-    }catch(err){
-      setErrorMessage(err.Message);
-      setLoading(false);
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
+    } catch (err) {
+      dispatch(signInFailure(data));
     }
-
-  }
+  };
 
   return (
-    <div className='min-h-screen mt-20'>
-      <div className='flex max-w-3xl flex-col md:mx-auto p-10 md:p-0 gap-5 md:gap-0 md:flex-row'>
+    <div className="min-h-screen mt-20">
+      <div className="flex max-w-3xl flex-col md:mx-auto p-10 md:p-0 gap-5 md:gap-0 md:flex-row">
         {/* left */}
-        <div className='flex-1 content-center'>
-        <span className=' mx-auto font-bold px-2 py-1 bg-gradient-to-r from-indigo-600 via-purple-500 to-pink-600 rounded-lg text-white text-xl md:text-4xl'>Prism</span>
-        <p className='mt-5 font-semibold'>Prism the truth with post</p>
+        <div className="flex-1 content-center">
+          <span className=" mx-auto font-bold px-2 py-1 bg-gradient-to-r from-indigo-600 via-purple-500 to-pink-600 rounded-lg text-white text-xl md:text-4xl">
+            Prism
+          </span>
+          <p className="mt-5 font-semibold">Prism the truth with post</p>
         </div>
         {/* right */}
-        <div className='flex-1'>
-          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-            
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
-              <Label value='Your username' />
-              <TextInput placeholder='Username' type='text' id='username' onChange={handleInputs}></TextInput>
+              <Label value="Your username" />
+              <TextInput
+                placeholder="Username"
+                type="text"
+                id="username"
+                onChange={handleInputs}
+              ></TextInput>
             </div>
 
             <div>
-              <Label value='Your Password' />
-              <TextInput placeholder='Password' type='password' id='password' onChange={handleInputs} rightIcon={FaEye}></TextInput>
+              <Label value="Your Password" />
+              <TextInput
+                placeholder="Password"
+                type="password"
+                id="password"
+                onChange={handleInputs}
+                rightIcon={FaEye}
+              ></TextInput>
             </div>
 
-            <div className='flex flex-col gap-2'>
-              <Button className='bg-gradient-to-r from-red-500 to-purple-500 font-bold' type='submit' disabled={loading} >
-                {loading ? <><Spinner></Spinner>'Loading...'</> :'Sign Up'}</Button>
+            <div className="flex flex-col gap-2">
+              <Button
+                className="bg-gradient-to-r from-red-500 to-purple-500 font-bold"
+                type="submit"
+                disabled={loading}
+              >
+                {loading ? (
+                  <>
+                    <Spinner></Spinner>'Loading...'
+                  </>
+                ) : (
+                  "Sign Up"
+                )}
+              </Button>
 
-              <Button className='bg-gradient-to-r from-blue-500 via-green-500 via-yellow-400 to-red-500 ' outline><FaGoogle className='mt-0 mx-2 text-xl'/> Continue With Google</Button>
-              <span className='flex gap-2 text-center mt-0'>
-              <p>Already Have an Account ! <Link to="/sign-in" className='text-blue-700 font-semibold m-0 underline'>signIn</Link></p>
+              <Button
+                className="bg-gradient-to-r from-blue-500 via-green-500 via-yellow-400 to-red-500 "
+                outline
+              >
+                <FaGoogle className="mt-0 mx-2 text-xl" /> Continue With Google
+              </Button>
+              <span className="flex gap-2 text-center mt-0">
+                <p>
+                  Don't Have An Account ?{" "}
+                  <Link
+                    to="/sign-up"
+                    className="text-blue-700 font-semibold m-0 underline"
+                  >
+                    signUp
+                  </Link>
+                </p>
               </span>
             </div>
           </form>
-          { errorMessage != null && <Alert color='failure'>{errorMessage}</Alert> }
+          {errorMessage != null && (
+            <Alert color="failure">{errorMessage}</Alert>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 }
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,75 +1,79 @@
-import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export const signup = async (req, res, next) => {
-    // res.json(req.body);
+  // res.json(req.body);
 
-    const {username,email,password} = req.body;
+  const { username, email, password } = req.body;
 
-    if(!username || !email || !password || username == "" || password == "" || email == ""){
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username == "" ||
+    password == "" ||
+    email == ""
+  ) {
+    // res.status(400).json({"message":"All fields Are Required !"});
+    next(errorHandler(400, "All fields are required"));
+  }
 
-        // res.status(400).json({"message":"All fields Are Required !"});
-        next(errorHandler(400, "All fields are required"));
-    }
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    const hashedPassword = bcryptjs.hashSync(password,10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
-    const newUser = new User({
-        username,
-        email,
-        password:hashedPassword
-    });
+  try {
+    await newUser.save();
+    res.json("Account Created");
+  } catch (err) {
+    // const errMsg = `${Object.keys(err.keyPattern)} is already in use`;
+    // err.message = errMsg;
 
+    next(err);
+  }
 
-    try{
-        await newUser.save();
-        res.json("Account Created");
-    }catch(err){
-        // const errMsg = `${Object.keys(err.keyPattern)} is already in use`;
-        // err.message = errMsg;
-        
-        next(err);
-    }
-
-    //task->
-    // validate userInfo username email password
-    // save to database
-    // hashing password using bcryptjs
+  //task->
+  // validate userInfo username email password
+  // save to database
+  // hashing password using bcryptjs
 };
 
-export const signin = async (req,res,next)=>{
-    const {username, password} = req.body;
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
 
-    if(!username || !password || username == '' || password == ''){
-        return next(errorHandler(400,'No username or Password Entered'));
+  if (!username || !password || username == "" || password == "") {
+    return next(errorHandler(400, "No username or Password Entered"));
+  }
+
+  try {
+    const validUser = await User.findOne({ username });
+    if (!validUser) {
+      return next(errorHandler(400, `No such Username ${username} found !`));
     }
 
-    try{
-        const validUser = await User.findOne({username});
-        if(!validUser){
-            return next(errorHandler(400,`No such Username ${username} found !`));
-        }
-
-
-        const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if(!validPassword){
-            return next(errorHandler(400, "Incorrect Username or Password"))
-        }
-
-        const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
-
-        const {password:hashedPassword, ...rest} = validUser._doc;
-
-        res.status(200).cookie('access_token',token,{httpOnly:true}).json(rest);
-
-
-
-    }catch(error){
-        console.log(error);
-        return next(error);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Incorrect Username or Password"));
     }
-}
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: hashedPassword, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+};
